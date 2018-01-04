a284f15a0eff736a291702c0041438da76f33ec7
Felix Gnass <fgnass@gmail.com>
2018-01-04 12:47:48 +0100

    add menu items to access the keyboard shortcuts

--- main.js
+++ main.js
@@ -17,23 +17,57 @@ app.on('ready', () => {
   });
   const wc = win.webContents;
 
-  var template = [{
-    label: 'Google Calendar',
-    submenu: [
-      { label: 'Toggle Sidebar', click: () => { wc.executeJavaScript(`document.querySelector('.gb_ec').click()`) }},
-      { label: 'Quit', accelerator: 'Command+Q', click: () => { app.quit(); }},
-      { label: 'Developer Tools', accelerator: 'Command+Alt+J', click: () => { win.openDevTools(); }}
-    ]}, {
-    label: 'Edit',
-    submenu: [
-      { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
-      { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
-      { type: 'separator' },
-      { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
-      { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
-      { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
-      { label: 'Select All', accelerator: 'Command+A', selector: 'selectAll:' }
-    ]}
+  function sendEscape(keyCode) {
+    return () => {
+      wc.sendInputEvent({ type: 'char', keyCode: 'Esc' });
+      wc.sendInputEvent({ type: 'char', keyCode });
+    }
+  }
+
+  function click(selector) {
+    return () => {
+      wc.executeJavaScript(`document.querySelector('${selector}').click()`);
+    }
+  }
+  
+  var template = [
+    {
+      label: 'Google Calendar',
+      submenu: [
+        { label: 'Settings', accelerator: 'Command+,', click: sendEscape('s')},
+        { label: 'Quit', accelerator: 'Command+Q', click: () => { app.quit(); }},
+        { label: 'Developer Tools', accelerator: 'Command+Alt+J', click: () => { win.openDevTools(); }}
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'New Event', accelerator: 'Command+N', click: sendEscape('c') },
+        { label: 'Find', accelerator: 'Command+F', click: sendEscape('/') },
+        { label: 'Goto to Today', accelerator: 'Command+T', click: sendEscape('t') },
+        { type: 'separator' },
+        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'Command+A', selector: 'selectAll:' }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        { label: 'Day View', accelerator: 'Command+1', click: sendEscape('1') },
+        { label: 'Week View', accelerator: 'Command+2', click: sendEscape('2') },
+        { label: 'Month View', accelerator: 'Command+3', click: sendEscape('3') },
+        { label: 'Custom View', accelerator: 'Command+4', click: sendEscape('4') },
+        { label: 'Schedule View', accelerator: 'Command+5', click: sendEscape('5') },
+        { label: 'Year View', accelerator: 'Command+6', click: sendEscape('6') },
+        { type: 'separator' },
+        { label: 'Toggle Sidebar', accelerator: 'Command+.', click: click('.gb_ec') }
+      ]
+    }
   ];
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
