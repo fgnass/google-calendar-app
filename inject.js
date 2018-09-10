d8f2826904debe74ae0d27d7a75c035f6d3fc5a6
kevincoleman <kevin.n.coleman@gmail.com>
2018-09-10 13:16:23 -0700

    Modify page to allow account addition & switching

--- inject.js
+++ inject.js
@@ -18,7 +18,24 @@ function patch(win) {
 // Monkey-patch the main window as well as all iframes
 patch(window);
 Array.prototype.slice
-  .call(document.querySelectorAll('iframe'))
+  .call(document.querySelectorAll("iframe"))
   .forEach(function(f) {
     if (~f.src.indexOf(window.location.origin)) patch(f.contentWindow);
   });
+
+// Remove _blank from the account menu controls, to allow sign-in and switching of multiple accounts
+let targetNode = document.querySelector(
+  'div[aria-label="Account Information"]'
+);
+let mutateConfig = { attributes: true, childList: true, subtree: true };
+let mutateCallback = () => {
+  document
+    .querySelectorAll(
+      'div[aria-label="Account Information"] a[target="_blank"]'
+    )
+    .forEach(el => {
+      el.removeAttribute("target");
+    });
+};
+let observer = new MutationObserver(mutateCallback);
+observer.observe(targetNode, mutateConfig);
